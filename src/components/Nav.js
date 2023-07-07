7f6b63c7e77338c29fc8490bfbeacb9562fa3e19
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-07-07 19:56:16 +0330

    start project

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboardData , BsBriefcase , BsChatSquare } from "react-icons/bs";
+
+import {Link} from 'react-scroll'
+
+const Nav = () => {
+  return <nav>
+    <div className="container mx-autO">
+
+    </div>
+  </nav>;
+};
+
+export default Nav;
