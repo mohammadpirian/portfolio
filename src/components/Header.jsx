fc78c79aec123604694b0de9a4e6aa17c98f97b2
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-07-08 01:23:10 +0330

    create banner and add motion to components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.svg";
 
 const Header = () => {
   return (
-    <header className=" py-8">
+    <header className="py-8">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <a href="#">
