09975c4e0a055d80088c0aa6d9712c9b532d0c40
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-07-09 12:51:19 +0330

    create nextmodal component

--- src/components/modals/NextModal.jsx
+++ src/components/modals/NextModal.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 
-const NextModal = () => {
+const NextModal = ({ nextModal, setNextModal }) => {
   return (
     <div>
       return (
       <div
-        className="fixed z-50 top-0 overflow-x-hidden w-full backdrop-blur-sm h-screen flex items-center justify-center"
-        // onClick={(e) =>
-        //   e.target == e.currentTarget && setCotactModal(!contactModal)
-        // }
+        className="fixed z-50 top-0 left-0 overflow-x-hidden w-full backdrop-blur-sm h-screen flex items-center justify-center"
+        onClick={(e) => e.target == e.currentTarget && setNextModal(!nextModal)}
       >
         <div className="border rounded-2xl w-8/12 p-1">
           <div className=" bg-white/80 rounded-2xl text-accent2/90 flex flex-col gap-y-6 p-6 font-primary font-semibold text-[18px] leading-[30px] lg:me-h3">
-            <p>Name: MOHAMMAD</p>
-            <p>Last Name: PIRIYAN</p>
+            <p>Technology: Next.js</p>
             <p>
-              Phone Number:
-              <br className="lg:hidden" /> +98 938 573 6655
+              description: Pages Router, App Router, Layout, SSR, SSG,
+              Middleware.
             </p>
-            <p>Email: @mohammadpiriyan</p>
           </div>
         </div>
       </div>
